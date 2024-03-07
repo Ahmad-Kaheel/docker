aa68406996d3811ea6ea7ea8b140ed24e399a2c5
Ahmad Kaheel <ahmadkahil596@gmail.com>
2024-03-07 15:05:28 +0300

    my commit

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ import os
 app = Flask(__name__)
 
 if 'POSTGRES_PASSWORD_FILE' in os.environ:
-   with open(os.environ['POSTGRES_PASSWORD_FILE'], 'r') as f:
-       password = f.read().strip()
+    with open(os.environ['POSTGRES_PASSWORD_FILE'], 'r') as f:
+        password = f.read().strip()
 else:
-   password = os.environ['POSTGRES_PASSWORD']
+    password = os.environ['POSTGRES_PASSWORD']
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Docker!'
+    return 'Hello, Docker'
 
 
 @app.route('/widgets')
